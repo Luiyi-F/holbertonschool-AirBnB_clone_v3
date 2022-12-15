557d99202f9da487e0cfd3ee7bc1e5b0c9c3a859
Luiyi_FHB <luiyifernando5@gmail.com>
2022-12-15 15:30:27 -0500

    [TASK 5]: create new endpoint for create the dictionary for see the count of the all classes

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,12 +2,31 @@
 """index file for the RestAPI"""
 from api.v1.views import app_views
 from flask import Flask, jsonify
+from models import storage
+
+hbnb_d = {
+    "amenities": "Amenity",
+    "cities": "City",
+    "places": "Place",
+    "reviews": "Review",
+    "states": "State",
+    "users": "User"
+}
 
 
 @app_views.route("/status", strict_slashes=False)
 def hbnbstatus():
     """status hbnb"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route("/stats", strict_slashes=False)
+def hbnbstats():
+    """stats hbnb"""
+    stats_dict = {}
+    for key, value in hbnb_d.items():
+        stats_dict[key] = storage.count(value)
+    return jsonify(stats_dict)
 
 
 if __name__ == "__main__":
