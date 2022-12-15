03ca4d6785a5193c3e4ee546c3f074e3f8eead12
Luiyi_FHB <luiyifernando5@gmail.com>
2022-12-15 14:48:29 -0500

    [TASK 4]: create begin with creation restAPi

---
+++ api/v1/views/index.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+from api.v1.views import app_views
+from flask import Flask, jsonify
+
+
+@app_views.route("/status", strict_slashes=False)
+def hbnbstatus(execption):
+    """status hbnb"""
+    return jsonify({"status": "ok"})
+
+
+if __name__ == "__main__":
+    pass
