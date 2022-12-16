1a7caf60b7cec8c54d220132976a4e8a8f8f5bb7
nicolas1897 <106556299+nicolas1897@users.noreply.github.com>
2022-12-16 10:16:10 -0500

    updates

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,25 +1,34 @@
+
 #!/usr/bin/python3
-"""creation app"""
-from os import getenv
-from flask import Flask, make_response, jsonify
+"""app Flask api
+"""
+from flask import Flask, jsonify
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
+from os import getenv
+
+host = getenv('HBNB_API_HOST') or '0.0.0.0'
+port = getenv('HBNB_API_PORT') or 5000
 
 app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
+
+cors = CORS(app, resources={'*': {'origins': '0.0.0.0'}})
 
 
 @app.teardown_appcontext
-def teardown(code):
-    """teardown appcpmtext"""
+def close(self):
+    """Close session"""
     storage.close()
 
 
 @app.errorhandler(404)
-def notFound(error):
-    return make_response(jsonify({'error': 'Not found'}), 404)
+def page_not_found(error):
+    """ Page not found error 404 """
+    return jsonify({"error": "Not found"}), 404
 
 
-if __name__ == "__main__":
-    app.run(host=getenv("HBNB_API_HOST", "0.0.0.0"),
-            port=int(getenv("HBNB_API_PORT", "5000")), threaded=True)
+if __name__ == '__main__':
+    app.run(host=host, port=port, threaded=True)
